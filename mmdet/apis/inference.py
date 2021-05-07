9e7409fab7397cb5434e77475b65525e2be12127
seoilgun <seoilgun@gmail.com>
2021-05-07 18:47:47 +0900

    mig

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -238,3 +238,25 @@ def show_result_pyplot(model,
         win_name=title,
         bbox_color=(72, 101, 241),
         text_color=(72, 101, 241))
+
+def save_result_pyplot(model, img, result, output_path, score_thr=0.3, fig_size=(15, 10)):
+    """Visualize the detection results on the image.
+
+    Args:
+        model (nn.Module): The loaded detector.
+        img (str or np.ndarray): Image filename or loaded image.
+        result (tuple[list] or list): The detection result, can be either
+            (bbox, segm) or just bbox.
+        score_thr (float): The threshold to visualize the bboxes and masks.
+        fig_size (tuple): Figure size of the pyplot figure.
+    """
+    if hasattr(model, 'module'):
+        model = model.module
+    img = model.show_result(img, result, score_thr=score_thr, show=False, bbox_color='red',
+                            text_color='red',
+                            thickness=4,
+                            font_scale=0.75, )
+    plt.figure(figsize=fig_size)
+    plt.imshow(mmcv.bgr2rgb(img))
+    plt.savefig(output_path, format='jpg')
+    plt.cla()
