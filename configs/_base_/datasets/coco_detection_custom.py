def5e255d4a9e9919a299989f891aae7bb22f5d0
seoilgun <seoilgun@gmail.com>
2021-05-07 17:23:16 +0900

    mig

--- configs/_base_/datasets/coco_detection_custom.py
+++ configs/_base_/datasets/coco_detection_custom.py
@@ -1,5 +1,5 @@
 # dataset settings
-dataset_type = 'FashionDataset'
+dataset_type = 'PlatnDataset'
 data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=[(1333, 800), (800, 1333), (800, 800)],
+        img_scale=[(800, 800)],
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
