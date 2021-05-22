bfd4b11cfb2bffb6433cebb07e94482bb64514c9
seoilgun <seoilgun@gmail.com>
2021-05-22 16:38:15 +0900

    mig

--- configs/detectors/detectors_cascade_rcnn_r50_1x_coco_dogeyenose.py
+++ configs/detectors/detectors_cascade_rcnn_r50_1x_coco_dogeyenose.py
@@ -82,4 +82,14 @@ data = dict(
         classes=classes, pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
 
-optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+# optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='Adam', lr=0.0005)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[180])
+runner = dict(type='EpochBasedRunner', max_epochs=210)
