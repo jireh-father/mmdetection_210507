1bb17b27158ad3b56a71592cecbe22ce31397721
seoilgun <seoilgun@gmail.com>
2021-05-24 15:40:34 +0900

    mig

--- demo/images_demo_dogeyenose.py
+++ demo/images_demo_dogeyenose.py
@@ -6,6 +6,7 @@ import os
 import time
 import shutil
 from PIL import Image
+import numpy as np
 
 def is_intersect(a, b):  # returns None if rectangles don't intersect
     dx = min(a[2], b[2]) - max(a[0], b[0])
@@ -47,6 +48,31 @@ def main():
         print(img)
 
         output_path = os.path.join(args.output_dir, 'vis', os.path.splitext(os.path.basename(img))[0] + ".jpg")
+        print(result)
+        eye_scors = result[0][:, 4]
+        eye_scors_indices = np.argsort(eye_scors)
+        eye_scors_indices = eye_scors_indices[::-1]
+        highest_score_eye = result[0][eye_scors_indices[0]]
+        second_score_eye = None
+        second_score_eye_idx = None
+        for idx in range(1, len(eye_scors_indices)):
+            if not is_intersect(highest_score_eye, result[0][eye_scors_indices[idx]]):
+                second_score_eye = result[0][eye_scors_indices[idx]]
+                second_score_eye_idx = idx
+                break
+        if second_score_eye is None and len(result[0]) > 1:
+            second_score_eye_idx = 1
+
+        if second_score_eye_idx:
+            result[0] = result[0][[0, second_score_eye_idx], :]
+        else:
+            result[0] = result[0][:1, :]
+
+        max_nose_idx = np.argmax(result[1][:, 4])
+        result[1] = result[1][max_nose_idx:max_nose_idx + 1, :]
+
+        print(result)
+        sys.exit()
         save_result_pyplot(model, img, result, output_path, score_thr=args.score_thr)
         im = Image.open(img).convert("RGB")
         for j, bbox in enumerate(result[0]):
