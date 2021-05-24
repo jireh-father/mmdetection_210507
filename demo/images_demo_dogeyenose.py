f66feaa142ef91e2aad5419e19189930966e7921
seoilgun <seoilgun@gmail.com>
2021-05-24 15:46:49 +0900

    mig

--- demo/images_demo_dogeyenose.py
+++ demo/images_demo_dogeyenose.py
@@ -8,6 +8,7 @@ import shutil
 from PIL import Image
 import numpy as np
 
+
 def is_intersect(a, b):  # returns None if rectangles don't intersect
     dx = min(a[2], b[2]) - max(a[0], b[0])
     dy = min(a[3], b[3]) - max(a[1], b[1])
@@ -42,36 +43,35 @@ def main():
         result = inference_detector(model, img)
         print(time.time() - start)
         # show the results
-        if len(result) < 1 or len(result[0]) < 1:
+        if len(result) < 1 or (len(result[0]) < 1 and len(result[1]) < 1):
             shutil.copy(img, os.path.join(args.output_dir, 'nodetected'))
             continue
-        print(img)
 
         output_path = os.path.join(args.output_dir, 'vis', os.path.splitext(os.path.basename(img))[0] + ".jpg")
-        print(result)
-        eye_scors = result[0][:, 4]
-        eye_scors_indices = np.argsort(eye_scors)
-        eye_scors_indices = eye_scors_indices[::-1]
-        highest_score_eye = result[0][eye_scors_indices[0]]
-        second_score_eye = None
-        second_score_eye_idx = None
-        for idx in range(1, len(eye_scors_indices)):
-            if not is_intersect(highest_score_eye, result[0][eye_scors_indices[idx]]):
-                second_score_eye = result[0][eye_scors_indices[idx]]
-                second_score_eye_idx = idx
-                break
-        if second_score_eye is None and len(result[0]) > 1:
-            second_score_eye_idx = 1
+        if len(result[0]) > 0:
+            eye_scors = result[0][:, 4]
+            eye_scors_indices = np.argsort(eye_scors)
+            eye_scors_indices = eye_scors_indices[::-1]
+            highest_score_eye = result[0][eye_scors_indices[0]]
+            second_score_eye = None
+            second_score_eye_idx = None
+            for idx in range(1, len(eye_scors_indices)):
+                if not is_intersect(highest_score_eye, result[0][eye_scors_indices[idx]]):
+                    second_score_eye = result[0][eye_scors_indices[idx]]
+                    second_score_eye_idx = idx
+                    break
+            if second_score_eye is None and len(result[0]) > 1:
+                second_score_eye_idx = 1
 
-        if second_score_eye_idx:
-            result[0] = result[0][[0, second_score_eye_idx], :]
-        else:
-            result[0] = result[0][:1, :]
+            if second_score_eye_idx:
+                result[0] = result[0][[0, second_score_eye_idx], :]
+            else:
+                result[0] = result[0][:1, :]
 
-        max_nose_idx = np.argmax(result[1][:, 4])
-        result[1] = result[1][max_nose_idx:max_nose_idx + 1, :]
+        if len(result[1]) > 0:
+            max_nose_idx = np.argmax(result[1][:, 4])
+            result[1] = result[1][max_nose_idx:max_nose_idx + 1, :]
 
-        print(result)
         save_result_pyplot(model, img, result, output_path, score_thr=args.score_thr)
         im = Image.open(img).convert("RGB")
         for j, bbox in enumerate(result[0]):
