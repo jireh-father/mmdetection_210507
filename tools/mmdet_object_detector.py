590151590b906c4acb1ff94a96db243f6bcddafb
jireh-father <shzk7399((99((>
2021-06-06 02:15:25 +0900

    dd

--- tools/mmdet_object_detector.py
+++ tools/mmdet_object_detector.py
@@ -5,32 +5,93 @@ import os
 import glob
 
 
+def save_result_pyplot(model,
+                       img,
+                       result,
+                       score_thr=0.3,
+                       title='result',
+                       out_file=None,
+                       mask_color=None,
+                       bbox_color='red',
+                       text_color='red',
+                       thickness=4):
+    """Visualize the detection results on the image.
+
+    Args:
+        model (nn.Module): The loaded detector.
+        img (str or np.ndarray): Image filename or loaded image.
+        result (tuple[list] or list): The detection result, can be either
+            (bbox, segm) or just bbox.
+        score_thr (float): The threshold to visualize the bboxes and masks.
+        title (str): Title of the pyplot figure.
+        wait_time (float): Value of waitKey param.
+                Default: 0.
+    """
+    if hasattr(model, 'module'):
+        model = model.module
+    model.show_result(
+        img,
+        result,
+        score_thr=score_thr,
+        show=False,
+        out_file=out_file,
+        win_name=title,
+        bbox_color=bbox_color,
+        text_color=text_color,
+        thickness=thickness,
+        mask_color=mask_color)
+
+
 class MmdetObjectDetector:
     def __init__(self, config, checkpoint, device='CPU'):
         self.model = init_detector(config, checkpoint, device=device)
 
     def inference(self, image_file_or_array):
         result = inference_detector(self.model, image_file_or_array)
+        # result[0]: bounding box
+        # result[0][0]: class 0's bboxes
+        # result[0][0][0]: [x1,y1,x2,y2,score]
+        # result[1]: masks
+        # result[1][0]: class 0's mask
+        # result[1][0][0]: masks
         print(result[0][0])
         print(result[1][0][0])
         print(result[0][0].shape)
         print(result[1][0][0].shape)
+        return result
     #
     # def async_inference(self, image_files_or_arrays):
     #     tasks = asyncio.create_task(async_inference_detector(self.model, image_files_or_arrays))
     #     result = await asyncio.gather(tasks)
     #     print(result)
 
+
 def main(args):
     od = MmdetObjectDetector(args.config, args.checkpoint, args.device)
     if os.path.isfile(args.img_or_pattern):
-        imgs = args.img_or_pattern
+        imgs = [args.img_or_pattern]
     else:
         imgs = glob.glob(args.img_or_pattern)
     # if args.async_test:
     #     od.async_inference(imgs)
     # else:
-    od.inference(imgs)
+    result = od.inference(imgs)
+    if args.show_dir:
+        os.makedirs(args.show_dir, exist_ok=True)
+
+    for img in imgs:
+        if args.show_dir:
+            out_file = os.path.join(args.show_dir, os.path.splitext(os.path.basename(img))[0] + ".jpg")
+        save_result_pyplot(od.model,
+                           img,
+                           result,
+                           score_thr=args.score_thr,
+                           out_file=out_file,
+                           # mask_color=None,
+                           # bbox_color='red',
+                           # text_color='red',
+                           # thickness=4
+                           )
 
 
 def parse_args():
@@ -42,6 +103,7 @@ def parse_args():
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--score-thr', type=float, default=0.3, help='bbox score threshold')
+    parser.add_argument('--show_dir', default=None, type=str)
     # parser.add_argument(
     #     '--async-test',
     #     action='store_true',
