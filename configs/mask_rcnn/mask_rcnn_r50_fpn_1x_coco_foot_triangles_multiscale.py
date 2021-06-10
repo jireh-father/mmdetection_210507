fd6ab87ce969e7ae99aa23cab289e1c4dda3e7a5
seoilgun <seoilgun@gmail.com>
2021-06-10 21:40:16 +0900

    mig

---
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_foot_triangles_multiscale.py
@@ -0,0 +1,61 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn_class3.py',
+    # '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+# dataset settings
+dataset_type = 'FootTriangles'
+data_root = 'data/coco/foot_triangles/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    # dict(type='Resize', img_scale=(416, 416), keep_ratio=True),
+    dict(type='Rotate', level=1, img_fill_val=[0, 0, 0], max_rotate_angle=15),
+    dict(type='PhotoMetricDistortion'),
+    dict(
+        type='Resize',
+        img_scale=[(400, 380), (700, 430)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(600, 425),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.0),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/train.json',
+        img_prefix=data_root + 'train_images/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'val_images/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'val_images/',
+        pipeline=test_pipeline))
+evaluation = dict(metric=['bbox', 'segm'])
